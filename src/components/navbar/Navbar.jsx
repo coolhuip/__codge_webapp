be33473e8464c05b1899d818d365f745c337d791
Danny Han <cool.huip@gmail.com>
2023-04-09 03:11:49 -0400

    imported assets + updated Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/slack.png';
 import './navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="codge__navbar">
+      <div className="codge__navbar">
+        <div className="codge__navbar-links_logo">
+          <img src={logo} alt="logo" /> 
+        </div>
+      </div>
+    </div>
   )
 }
 
